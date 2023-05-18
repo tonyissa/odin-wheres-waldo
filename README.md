8dd5b911546afdf70c66da3910fbf09cbedccb74
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-05-17 23:15:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# odin-react-eslint-prettier
+Blank template for eslint, prettier, and react. Clone this when you want to make a new project
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import '../style/App.css';
+
+export default function App() {
+    return (
+      <div id="app">
+
+      </div>
+    );
+}
