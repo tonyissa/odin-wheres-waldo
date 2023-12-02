65f611e421dafef487f84c3c2d6b108744cb4149
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-12-02 09:58:09 -0500

    Reafactor code to prepare for backend operations

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 
-export default function Dropdown({ toggleLeaderboard, time, setTime, getScore }) {
+export default function Dropdown({ toggleLeaderboard, time, nextState, getScore }) {
     const [name, setName] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -13,7 +13,7 @@ export default function Dropdown({ toggleLeaderboard, time, setTime, getScore })
             const response = await fetch('', {
                 mode: 'cors',
                 method: 'post',
-                body: { name }
+                body: JSON.stringify({ name, time })
             });
             setLoading(false);
             if (response.status === 200) {
@@ -34,7 +34,7 @@ export default function Dropdown({ toggleLeaderboard, time, setTime, getScore })
         </div>
         <div id='button-container'>
             <button onClick={handleSubmit} disabled={loading}>Submit score</button>
-            <button id="again" disabled={loading}>Start again</button>
+            <button id="again" onClick={nextState} disabled={loading}>Start again</button>
         </div>
         <div className='error-text'>{error}</div>
     </>;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,25 +2,41 @@
 import React from 'react';
 import collage from '../assets/collage.jpg';
 
-export default function Game(props) {
+export default function Game({ nextState }) {
 
-    function handleClick(e) {
-        if (props.round === 1) {
-            if (e.nativeEvent.offsetX >= 685 && e.nativeEvent.offsetX <= 732 &&
-                e.nativeEvent.offsetY >= 682 && e.nativeEvent.offsetY <= 746) {
-                    props.nextState();
-                }
-        } else if (props.round === 2) {
-            if (e.nativeEvent.offsetX >= 155 && e.nativeEvent.offsetX <= 211 &&
-                e.nativeEvent.offsetY >= 669 && e.nativeEvent.offsetY <= 726) {
-                    props.nextState();
-                }
-        } else {
-            if (e.nativeEvent.offsetX >= 22 && e.nativeEvent.offsetX <= 134 &&
-                e.nativeEvent.offsetY >= 310 && e.nativeEvent.offsetY <= 461) {
-                    props.nextState();
-                }
+    async function handleClick(e) {
+        try {
+            const response = await fetch('', {
+                mode: 'cors',
+                method: 'post',
+                body: JSON.stringify({ 
+                    XCoords: e.nativeEvent.offsetX,
+                    YCoords: e.nativeEvent.offsetY
+                })
+            });
+            if (response.status === 200) {
+                nextState();
+            }
+        } catch (err) {
+            console.error(err);
         }
+
+        // if (props.round === 1) {
+        //     if (e.nativeEvent.offsetX >= 685 && e.nativeEvent.offsetX <= 732 &&
+        //         e.nativeEvent.offsetY >= 682 && e.nativeEvent.offsetY <= 746) {
+        //             props.nextState();
+        //         }
+        // } else if (props.round === 2) {
+        //     if (e.nativeEvent.offsetX >= 155 && e.nativeEvent.offsetX <= 211 &&
+        //         e.nativeEvent.offsetY >= 669 && e.nativeEvent.offsetY <= 726) {
+        //             props.nextState();
+        //         }
+        // } else {
+        //     if (e.nativeEvent.offsetX >= 22 && e.nativeEvent.offsetX <= 134 &&
+        //         e.nativeEvent.offsetY >= 310 && e.nativeEvent.offsetY <= 461) {
+        //             props.nextState();
+        //         }
+        // }
     }
 
     return <div id="game">
